d951d36b63909ce51f645204b34f5337d8d5f0ae
Pavel-Gornov <gornovpavel30@yandex.ru>
2026-02-20 11:49:56 +0400

    Работоспособная вставка в дерево

--- TreeSet.h
+++ TreeSet.h
@@ -1,13 +1,14 @@
 #ifndef TREESET_H
 #define TREESET_H
 
-#include <ostream>
+#include <iostream>
 
 template<typename T>
 class TreeSet {
 private:
     struct TreeNode {
         TreeNode(T value): left(nullptr), value(value), right(nullptr), height_(1) {};
+        ~TreeNode() {std::cout << value << " deleted ";}
         TreeNode* left;
         T value;
         TreeNode* right;
@@ -18,68 +19,155 @@ public:
 
     TreeSet(const TreeSet<T>& tset) = delete;
     TreeSet<T>& operator=(const TreeSet<T>& tset) = delete;
-    ~TreeSet() = delete;
+    ~TreeSet() {
+        destroy_(root_);
+    }
 
-    //void print() const; // По заданию нужно -_-
+    size_t size() const { return size_; }
+    size_t height() const { return height(root_);}
+
+    void print() const {
+        print_(root_);
+        std::cout << "\n";
+    }
+
     bool insert(T key) {
-        if (!root_) {
-            root_ = new TreeNode<T>(key);
-            size_ = 1;
-            return true;
-        }
-        TreeNode* cur = root_;
-        //TODO: Доделать
-        
+        if (this->contains(key)) return false;
+
+        root_ = insert_(root_, key);
+        // if (!root_) {
+        //     root_ = new TreeNode(key);
+        //     size_ = 1;
+        //     return true;
+        // }
+        // TreeNode* cur = root_;
+        // while (cur) {
+        //     if (cur->value == key) return false;
+        //     if (!cur->left && !cur->right) break;
+
+        //     if (cur->value > key && cur->left) {
+        //         cur = cur->left; continue;
+        //     }
+        //     if (cur->value < key && cur->right) {
+        //         cur = cur->right; continue;
+        //     }
+        //     break;
+        // }
+
+        // if (cur->value > key) cur->left = new TreeNode(key);
+        // else cur->right = new TreeNode(key);
+ 
+        size_++;
         return true;
     };
 
-    bool contains(T key) const;
-    bool erase(T key);
-    bool strictly_balanced() const;
+    bool contains(T key) const {
+        const TreeNode* cur = root_;
+        while (cur) {
+            if (cur->value == key) return true;
+
+            if (cur->value > key) cur = cur->left;
+            else cur = cur->right;
+        }
+        return false;
+    }
+
+    // bool erase(T key);
+    // bool strictly_balanced() const;
 
 private:
     TreeNode* root_ = nullptr;
     size_t size_ = 0;
 
     static unsigned int height(const TreeNode* node) {
-        return (node ? node->height_: 0)
+        return (node ? node->height_: 0);
     }
 
     static unsigned int bfactor(const TreeNode* node) {
-        return height(node->right)-height(node->left);
+        return height(node->right) - height(node->left);
     }
 
-    void fix_height_(TreeNode* node) {
+    static void fix_height(TreeNode* node) {
         unsigned int hl = height(node->left);
         unsigned int hr = height(node->right);
         node->height_ = (hl > hr? hl: hr) + 1;
     }
 
-    void rotate_left_(TreeNode* node) {
+    static TreeNode* rotate_left(TreeNode* node) {
         TreeNode* r = node->right;
         node->right = r->left;
         r->left = node;
-        // TODO: Работа с высотами
+
+        fix_height(r);
+        fix_height(node);
+        return r;
     }
 
-    void rotate_right_(TreeNode* node) {
+    static TreeNode* rotate_right(TreeNode* node) {
         TreeNode* l = node->left;
         node->left = l->right;
         l->right = node;
-        // TODO: Работа с высотами
+
+        fix_height(l);
+        fix_height(node);
+        return l;
     }
 
-    void big_rotate_left_(TreeNode<T>* node) {
-        rotate_right_(node->right);
-        rotate_left_(node);
+    static TreeNode* balance_node(TreeNode* node) {
+        fix_height(node);
+        if (bfactor(node) == 2) {
+            if (bfactor(node->right) < 0)
+                node->right = rotate_right(node->right);
+            return rotate_left(node);
+        }
+        if (bfactor(node) == -2) {
+            if (bfactor(node->left) > 0)
+                node->left = rotate_left(node->left);
+            return rotate_right(node);
+        }
+        return node;
+    };
+
+    // void big_rotate_left_(TreeNode* node) {
+    //     rotate_right_(node->right);
+    //     rotate_left_(node);
+    // }
+
+    // void big_rotate_right_(TreeNode* node) {
+    //     rotate_left(node->right);
+    //     rotate_right_(node);
+    // }
+
+    static TreeNode* insert_(TreeNode* node, T key) {
+        if (!node) return new TreeNode(key);
+        if (node->value > key)
+            node->left = insert_(node->left, key);
+        else
+            node->right = insert_(node->right, key);
+        return balance_node(node);
     }
 
-    void big_rotate_right_(TreeNode<T>* node) {
-        rotate_left(node->right);
-        rotate_right_(node);
-    }
+    static void destroy_(const TreeNode* root) {
+        if (!root) return;
+        destroy_(root->left);
+        destroy_(root->right);
+        delete root;
+    };
 
-    void balance_tree_() {};
+    static void print_(const TreeNode* node) { // TODO: Сделать нагляднее
+        if (!node) return;
+        std::cout << "(" << node->value << ")";
+        if (node->left) {
+            std::cout << " <";
+            print_(node->left);
+            std::cout << ">, ";
+        }
+        if (node->right) {
+            std::cout << " [";
+            print_(node->right);
+            std::cout << "]";
+        }
+    }
 };
 
 // template<typename T>
--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include "TreeSet.h"
 
 int main() {
-    std::cout << "Hello World\n";
+    TreeSet<int> tset = TreeSet<int>();
+    for (int i = 0; i < 12; i++) {
+        tset.insert(i);
+    }
+    tset.print();
+    
+    std::cout << "size: " << tset.size() << "\nhight: " <<tset.height() << "\n";
 }
